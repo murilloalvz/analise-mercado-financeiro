784290aed8133917a819666d612ddc1dffb8ac47
Murillo Alves <murilloalveslou@outlook.com>
2026-08-04 20:48:32 -0300

    feat: display latest closing price

--- src/main.py
+++ src/main.py
@@ -13,14 +13,20 @@ def buscar_dados(ticker, periodo):
 
     return dados
 
-ticker = "PETR4.SA"
+ticker = "VALE3.SA"
 
-dados = buscar_dados(ticker, "6mo")
+dados = buscar_dados(ticker, "1y")
 
 if dados.empty:
     print("Nenhum dado foi encontrado.")
 else:
     print(f"Dados encontrados para {ticker}:")
     print(dados.head())
+
+    ultimo_fechamento = dados["Close"].iloc[-1].iloc[0]
+
+    print(f"\nÚltimo fechamento: R$ {ultimo_fechamento:.2f}")
     print(f"\nTotal de registros: {len(dados)}")
+
+
 
