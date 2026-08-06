11ce6ac59dccab78b3e7a4f836da1df744450a48
Murillo Alves <murilloalveslou@outlook.com>
2026-08-05 21:02:26 -0300

    refactor: organize financial analysis into functions

--- src/main.py
+++ src/main.py
@@ -13,6 +13,24 @@ def buscar_dados(ticker, periodo):
 
     return dados
 
+def calcular_retorno(dados):
+    primeiro_fechamento = dados["Close"].iloc[0]
+    ultimo_fechamento = dados["Close"].iloc[-1]
+    retorno = ((ultimo_fechamento - primeiro_fechamento) / primeiro_fechamento)  * 100
+
+    return primeiro_fechamento, ultimo_fechamento, retorno
+
+def adicionar_retorno_diario(dados):
+    dados["Retorno_Diario"] = dados["Close"].pct_change() * 100
+
+    return dados
+
+def calcular_estatisticas(dados):
+    media_retorno_diario = dados["Retorno_Diario"].mean()
+    volatilidade_diaria = dados["Retorno_Diario"].std()
+
+    return media_retorno_diario, volatilidade_diaria
+
 ticker = "VALE3.SA"
 
 dados = buscar_dados(ticker, "1y")
@@ -21,23 +39,18 @@ dados.columns = dados.columns.get_level_values(0)
 if dados.empty:
     print("Nenhum dado foi encontrado.")
 else:
-    print(f"Dados encontrados para {ticker}:")
-    print(dados.head())
+    primeiro_fechamento, ultimo_fechamento, retorno = calcular_retorno(dados)
+    dados = adicionar_retorno_diario(dados)
+    media_retorno_diario, volatilidade_diaria = calcular_estatisticas(dados)
 
-    primeiro_fechamento = dados["Close"].iloc[0]
-    ultimo_fechamento = dados["Close"].iloc[-1]
-    retorno = ((ultimo_fechamento - primeiro_fechamento) / primeiro_fechamento)  * 100
-    dados["Retorno_Diario"] = dados["Close"].pct_change() * 100
-    media_retorno_diario = dados["Retorno_Diario"].mean()
-    volatilidade_diaria = dados["Retorno_Diario"].std()
-
-    print(f"\nPrimeiro fechamento: R$ {primeiro_fechamento:.2f}")
+    print(f"\nAtivo {ticker}:")
+    print(f"Primeiro fechamento: R$ {primeiro_fechamento:.2f}")
     print(f"Último fechamento: R$ {ultimo_fechamento:.2f}")
     print(f"Retorno no peródo: {retorno:.2f}%")
     print(f"Média do retorno diário: {media_retorno_diario:.2f}%")
     print(f"Total de registros: {len(dados)}")
     print(f"Volatilidade diária: {volatilidade_diaria:.2f}%")
-    print(dados[["Close", "Retorno_Diario"]].tail())
+   
 
 
     
